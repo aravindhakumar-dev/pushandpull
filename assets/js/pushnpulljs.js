b7154c7173fa7191948f1d96d314ee8116ffd864
AravindhakumarG <117360996+aravindhakumar-dev@users.noreply.github.com>
2023-06-26 15:01:20 +0530

    started

---
+++ assets/js/pushnpulljs.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body');
+const elements = body.querySelectorAll('p, h2, li a');
+
+elements.forEach(element => {
+    const text = element.textContent;
+    const letters = text.split('');
+
+    element.textContent = '';
+
+    letters.forEach(letter => {
+        const span = document.createElement('span');
+        span.textContent = letter;
+        element.appendChild(span);
+
+        span.addEventListener('mouseover', () => {
+            span.style.color = '#EEC627';
+        });
+
+        span.addEventListener('mouseout', () => {
+            span.style.color = '';
+        });
+    });
+});
